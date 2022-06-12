5e44ed860ae1f0024169b58a1523e2ac74d2bc12
Ethan Kupka <ekupka23@cornellcollege.edu>
2022-06-12 15:41:34 -0600

    Added function making practice

--- fccpractice-spwpython/ex_02_03.py
+++ fccpractice-spwpython/ex_02_03.py
@@ -1,7 +1,42 @@
-#)2-03 EX pfe
+#)2-03 EX pfe - Calculating Pay
 
-xh = input('enter hours: ')
-xr = input('enter rate: ')
+sh = input('enter hours: ')
+sr = input('enter rate: ')
 
-xp = float(xh) * float(xr)
-print("pay: ", xp)
+try:
+    fh = float(sh) #Danger Code
+    fr = float(sr) #Danger Code
+except:
+    print("ERROR, Please enter a # input")
+    quit()
+
+# Check 1
+# print(fh, fr)
+
+# if fh > 40 :
+#     # print("Over-time")
+#     reg = fr * fh
+#     otp = (fh-40.0) * (fr*0.5)
+#     # print(reg, otp)
+#     xp = reg + otp
+# else:
+#     # print('Regular-time')
+#     xp = fr * fh
+
+# print("pay: ", xp)
+
+# 04-06 function making 
+def computePay(hours, rate):
+    print("In compute pay", hours, rate)
+    if hours > 40:
+        reg = fr * fh
+        otp = (fh-40.0) * (fr*0.5)
+        pay = reg + otp
+    else:
+        pay = fr * fh
+    # print("returning", pay)
+    return pay
+
+
+xp = computePay(fh, fr) #evokes function
+print("Pay: ", xp)
